0968ad251c422560b30f5bf79c3172d332f2185a
lucacm <lucacm@al.insper.edu.br>
2019-04-19 14:47:36 -0300

    adição de arquivos untracked

---
+++ Arquivo_Monstros_EP1.py
@@ -0,0 +1,55 @@
+monstros = [ 
+		{"Veterano": {
+			"fala": "Hahahaha, eu cursei anoooos de INSPER, acha mesmo que pode me derrotar?????",
+			"hit points": 10,
+			"pontos de ataque": 4,
+			"pontos de defesa": 2,
+			}
+		},
+		{"Bibliotecaria": {
+			"fala": "Ora se não é um forasteiro perdido procurando alguns livros.",
+			"hit points": 15,
+			"pontos de ataque": 5,
+			"pontos de defesa": 4
+			}
+		},
+		{"Fumante": {
+			"fala": "Cof... Cof... Se você veio pedir um trago, pode ir se preparando pra levar um pé na bunda",
+			"hit points": 5,
+			"pontos de ataque": 10,
+			"pontos de defesa": 1
+			}
+		},
+		{"Daniel Guzzo": {
+			"fala": "Você veio ao FAB LAB, pois bem QUERO UM PROTÓTIPO!",
+			"hit points": 50,
+			"pontos de ataque": 20,
+			"pontos de defesa": 10
+			}
+		},
+		{"Paulina": {
+			"fala": "Iterar e Graficar...",
+			"hit points": 65,
+			"pontos de ataque": 15,
+			"pontos de defesa": 20
+			}
+		}
+]
+
+
+	
+
+
+
+	
+
+
+
+
+
+
+
+		
+
+
+
